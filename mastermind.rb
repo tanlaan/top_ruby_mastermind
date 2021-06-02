1c2fcca80153e96d9d4bc3e59d039ffd3413c70e
Chris Bolas <tanlaan33@gmail.com>
2021-06-02 23:20:02 +0000

    Generic random secret gameplay complete

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,17 @@
-require_relative './board.rb'
+require_relative './tui.rb'
+require_relative './game.rb'
 # We will run this in the command line
 # Computer will choose 4 random digits
 # Player will be presented with 'console'
 #   for inputing guesses
 
+playing = true
+welcome_message()
+
+while playing
+    game = Game.new
+    game.start
+    playing = keep_playing?
+end
+
+exit_message()
